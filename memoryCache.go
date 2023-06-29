e6236c4d1473d21b46a04ae605d497b9c90e56ef
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-06-29 10:38:09 +0200

    get implemented

--- memoryCache.go
+++ memoryCache.go
@@ -1,14 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 // MemoryCache is the implementation of the Cache interface
 type MemoryCache struct {
+	data map[string][]byte
+	mtx  sync.RWMutex
 }
 
 // NewMemoryCache creates a new instance of the  MemoryCache
 func NewMemoryCache() *MemoryCache {
-	return &MemoryCache{}
+	return &MemoryCache{data: make(map[string][]byte), mtx: sync.RWMutex{}}
 }
 
+// Get returns the value stored wuth the given key, if the key does not exist
+// the fucntion returns an error
 func (c *MemoryCache) Get(key []byte) ([]byte, error) {
-	return []byte("OK"), nil
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
+	strKey := string(key)
+	value, ok := c.data[strKey]
+	if !ok {
+		return nil, fmt.Errorf("key (%s) does not exist", strKey)
+	}
+	return value, nil
 }
